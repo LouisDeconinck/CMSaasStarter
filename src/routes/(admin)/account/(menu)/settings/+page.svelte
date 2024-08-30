a53ca9d9029932d73130e3469472e32d80ee1d9b
Louis Deconinck <louis.dck@gmail.com>
2024-08-30 22:41:01 +0200

    cleaned up settings

--- src/routes/(admin)/account/(menu)/settings/+page.svelte
+++ src/routes/(admin)/account/(menu)/settings/+page.svelte
@@ -22,47 +22,11 @@
   fields={[
     { id: "fullName", label: "Name", initialValue: profile?.full_name ?? "" },
     {
-      id: "companyName",
-      label: "Company Name",
-      initialValue: profile?.company_name ?? "",
-    },
-    {
-      id: "website",
-      label: "Company Website",
-      initialValue: profile?.website ?? "",
+      id: "email",
+      label: "Email",
+      initialValue: user?.email || "",
     },
   ]}
-  editButtonTitle="Edit Profile"
-  editLink="/account/settings/edit_profile"
-/>
-
-<SettingsModule
-  title="Email"
-  editable={false}
-  fields={[{ id: "email", initialValue: user?.email || "" }]}
-  editButtonTitle="Change Email"
-  editLink="/account/settings/change_email"
-/>
-
-<SettingsModule
-  title="Password"
-  editable={false}
-  fields={[{ id: "password", initialValue: "••••••••••••••••" }]}
-  editButtonTitle="Change Password"
-  editLink="/account/settings/change_password"
-/>
-
-<SettingsModule
-  title="Email Subscription"
-  editable={false}
-  fields={[
-    {
-      id: "subscriptionStatus",
-      initialValue: profile?.unsubscribed ? "Unsubscribed" : "Subscribed",
-    },
-  ]}
-  editButtonTitle="Change Subscription"
-  editLink="/account/settings/change_email_subscription"
 />
 
 <SettingsModule
